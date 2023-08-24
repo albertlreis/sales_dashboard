a972bc1b97abbcb299f955a287bd620664b341eb
Albert Reis <albertlreis@gmail.com>
2023-08-24 11:30:27 -0300

    Listar produtos

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -12,11 +12,15 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class ProductController extends AbstractController
 {
-    #[Route('/product', name: 'app_product')]
-    public function index(): Response
+    /**
+     * @Route("/product", name="app_product_index")
+     */
+    public function index(EntityManagerInterface $entityManager): Response
     {
+        $products = $entityManager->getRepository(Product::class)->findAll();
+
         return $this->render('product/index.html.twig', [
-            'controller_name' => 'ProductController',
+            'products' => $products,
         ]);
     }
 
